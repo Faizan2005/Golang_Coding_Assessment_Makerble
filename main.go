f30adbaecbf3475a7d7558db091a200d43ef8326
Faizan2005 <illusionbuster05@gmail.com>
2025-06-10 19:48:00 +0530

    feat: start basic fiber server

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	routes "github.com/Faizan2005/Golang_Coding_Assessment_Makerble/routes"
+)
+
+func main() {
+	apiServer := routes.NewAPIServer(":3000")
+
+	apiServer.Run()
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,25 @@
 package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type APIServer struct {
+	listenAddr string
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *APIServer) Run() {
+	app := fiber.New()
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello from POST route!")
+	})
+
+	app.Listen(s.listenAddr)
+}
