91767b523376af43114baec702f84d6b6efd4712
Faizan2005 <illusionbuster05@gmail.com>
2025-06-11 16:06:26 +0530

    feat: add CRUD queries for patient

--- models/storage.go
+++ models/storage.go
@@ -1,6 +1,9 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type User struct {
 	ID       string `json:"id" db:"id"`
@@ -23,7 +26,7 @@ type Storage interface {
 	AddPatient(*Patient) error
 	GetPatients() ([]*Patient, error)
 	GetPatientByID(string) (*Patient, error)
-	UpdatePatientByID(string) error
+	UpdatePatient(string) error
 	DeletePatientByID(string) error
 }
 
@@ -35,4 +38,71 @@ func NewPostgresStore(db *sql.DB) (*PostgresStore, error) {
 	return &PostgresStore{
 		db: db,
 	}, nil
+}
+
+func (s *PostgresStore) AddPatient(p *Patient) error {
+
+	query := `INSERT INTO patients (
+	name, age, gender, diagnosis, created_by
+	)
+	VALUES ($1, $2, $3, $4, $5) 
+	RETURNING id`
+
+	err := s.db.QueryRow(query, p.Name, p.Age, p.Gender, p.Diagnosis, p.CreatedBy).Scan(&p.ID)
+	if err != nil {
+		return fmt.Errorf("Error inserting patient details: %v", err)
+	}
+	return nil
+}
+
+func (s *PostgresStore) GetPatients() ([]*Patient, error) {
+
+	query := `SELECT id, name, age, gender, diagnosis, created_by FROM patients`
+
+	rows, err := s.db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("Error fetching patients details: %v", err)
+	}
+
+	defer rows.Close()
+
+	var Patients []*Patient
+
+	for rows.Next() {
+		var p Patient
+
+		err := rows.Scan(&p.ID, &p.Name, &p.Age, &p.Gender, &p.Diagnosis, &p.CreatedBy)
+		if err != nil {
+			return nil, err
+		}
+
+		Patients = append(Patients, &p)
+	}
+	return Patients, nil
+}
+
+func (s *PostgresStore) GetPatientByID(id string) (*Patient, error) {
+	query := `SELECT id, name, age, gender, diagnosis, created_by FROM patients WHERE id=$1`
+
+	var p Patient
+
+	err := s.db.QueryRow(query, id).Scan(&p.ID, &p.Name, &p.Age, &p.Gender, &p.Diagnosis, &p.CreatedBy)
+	if err != nil {
+		return nil, fmt.Errorf("Error fetching patient details: %v", err)
+	}
+
+	return &p, nil
+}
+
+func (s *PostgresStore) UpdatePatient(p *Patient) error {
+	query := `UPDATE patients SET name=$1, age=$2, gender=$3, diagnosis=$4, created_by=$5`
+	_, err := s.db.Exec(query, p.Name, p.Age, p.Gender, p.Diagnosis, p.CreatedBy)
+	return err
+}
+
+func (s *PostgresStore) DeletePatientByID(id string) error {
+	query := `DELETE FROM patients WHERE id=$1`
+
+	_, err := s.db.Exec(query, id)
+	return err
 }
