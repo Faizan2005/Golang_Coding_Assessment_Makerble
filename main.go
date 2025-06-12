5f368b84c3129d6bc05baee8385792351bfcf161
Faizan2005 <illusionbuster05@gmail.com>
2025-06-12 11:33:10 +0530

    fix: changed main.go code

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	config "github.com/Faizan2005/Golang_Coding_Assessment_Makerble/config"
 	models "github.com/Faizan2005/Golang_Coding_Assessment_Makerble/models"
@@ -15,17 +16,26 @@ func main() {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
+	listenPort := os.Getenv("PORT")
+	if listenPort == "" {
+		listenPort = "3000"
+	}
+	listenAddr := ":" + listenPort
+
+	log.Printf("Application starting on %s", listenAddr)
+
 	db, err := config.ConnectDB()
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
+
+	defer db.Close()
 
 	store, err := models.NewPostgresStore(db)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	listenAddr := ":3000"
 	server := routes.NewAPIServer(listenAddr, store, store)
 	server.Run()
 }
